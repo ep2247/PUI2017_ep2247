264a83a03c3f7409fa79b46ab9918a1ef343c614
Emily Padvorac <emilypadvorac@172-30-22-132.DYNAPOOL.NYU.EDU>
2017-10-10 18:22:32 -0400

    upload all of assignment 4

---
+++ bootcamp/ispalindrome.py
@@ -0,0 +1,20 @@
+
+def lookForPal(string):
+    '''create a function that takes a string and checks if the string is a palindrome'''
+    revString = string[::-1]
+    print(revString)
+    
+    if string == revString:
+        print("Yes its a palindrome")
+        return int(True)
+    else:
+        print("This is not a palindrome")
+        return (False == -1)
+
+if string.lower() == string.lower()[::-1]
+    halfpal = string[:int((float(len(string)) / 2.0) +0.5 )]
+    print("Yes its a palindrome", halfpal)
+    return (1)
+else:
+    print("This is not a palindrome")
+    return (-1)
